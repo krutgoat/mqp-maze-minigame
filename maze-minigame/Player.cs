2af42e3e68d2b7e38293ac1e506180c608432d5f
krutgoat <krutics@outlook.com>
2024-11-08 00:40:52 -0500

    Movement
    right now, vertical movement is allowed and there are no border collisions (because i do not have hedge collisions defined). Player collisions should be defined, though.

--- maze-minigame/Player.cs
+++ maze-minigame/Player.cs
@@ -3,9 +3,24 @@ using System;
 
 public partial class Player : CharacterBody2D {
 	
-	private Vector2 currentVelocity; 
+	[Export]
+	private int speed = 200;
+	private Vector2 currentVelocity;
 	
-	public override void _PhysicsProcess(double delta) {
-		
+    public override void _PhysicsProcess(double delta) {
+        base._PhysicsProcess(delta);
+		handleInput(); // get currentVelocity
+		Velocity = currentVelocity; // update Velocity
+		MoveAndSlide();
+    }
+
+	// update velocity based on player input
+	private void handleInput() {
+		 currentVelocity = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		//  if Input.IsActionPressed("ui_left") {
+		// 	currentVelocity.X = -1;
+		//  }
+		 currentVelocity *= speed;
 	}
+
 }
